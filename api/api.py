879a1243ef04cca156ecc76eb46176451e69cf88
Christophe Blefari <christophe.blefari@gmail.com>
2025-01-21 16:24:44 +0100

    Add mistral

--- api/api.py
+++ api/api.py
@@ -2,6 +2,9 @@ import joblib
 import pandas as pd
 from flask import Flask, request, jsonify
 import json
+import os
+
+from mistralai import Mistral
 
 # To run the API, run `flask --app api --debug run`
 # Go to http://127.0.0.1:5000/api/gender to see the result
@@ -32,3 +35,44 @@ def gender():
         "name": name,
         "gender": gender,
     })
+
+@app.route("/api/gender/llm/")
+def gender_llm():
+    name = request.args.get('name', None)
+
+    if name is None:
+        return "Please provide a name", 400
+
+    MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+
+    client = Mistral(api_key=MISTRAL_API_KEY)
+
+    chat_response = client.chat.complete(
+        model="mistral-large-latest",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                You are a agent that have to guess the gender of a given name.
+                If a name is both, just return B.
+                
+                Examples:
+                For "Christophe" returns {"gender": "M"}.
+                For "Marie" returns {"gender": "F"}.
+                For "Camille" returns {"gender": "B"}.
+                
+                Just return a valid JSON response with no ```.
+                """,
+            },
+            {
+                "role": "user",
+                "content": name,
+            },
+        ]
+    )
+
+    try:
+        return jsonify(json.loads(chat_response.choices[0].message.content))
+    except Exception as error:
+        return jsonify({"error": "An error occured", "message": str(error)}), 200
+
